9f8d28a52f83f4ff4ae598cc148f6d9d7cb3e853
Jon Pickel <github@jonpickel.com>
2025-03-16 22:02:19 +0000

    Update keymap.c

--- DRK3q/keymap.c
+++ DRK3q/keymap.c
@@ -10,8 +10,8 @@ enum custom_keycodes {
   PARTY,
   ROFL,
   HUNDRED,
-  HORNS,
-  FINGERS_XED,
+  HORNS_LT,
+  FINGERS_XED_LT,
   QUESTION,
   SUCCESS,
   FAIL,
@@ -77,7 +77,7 @@ enum tap_dance_codes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
-    TD(DANCE_0),    THUMBS_UP_LT,   THUMBS_DOWN_LT, PARTY,          ROFL,           HUNDRED,                                        HORNS,          FINGERS_XED,    QUESTION,       SUCCESS,        FAIL,           KC_MINUS,       
+    TD(DANCE_0),    THUMBS_UP_LT,   THUMBS_DOWN_LT, PARTY,          ROFL,           HUNDRED,                                        HORNS_LT,       FINGERS_XED_LT, QUESTION,       SUCCESS,        FAIL,           KC_MINUS,       
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           TD(DANCE_2),    
     MO(1),          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOTE,       
     KC_TRANSPARENT, KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_BSLS,        
@@ -320,12 +320,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       SEND_STRING(SS_LALT(SS_TAP(X_KP_1) SS_TAP(X_KP_2) SS_TAP(X_KP_8) SS_TAP(X_KP_1) SS_TAP(X_KP_7) SS_TAP(X_KP_5) ));
     }
     break;
-    case HORNS:
+    case HORNS_LT:
     if (record->event.pressed) {
       SEND_STRING(SS_LALT(SS_TAP(X_KP_1) SS_TAP(X_KP_2) SS_TAP(X_KP_9) SS_TAP(X_KP_3) SS_TAP(X_KP_0) SS_TAP(X_KP_4) ) SS_LALT(SS_TAP(X_KP_1) SS_TAP(X_KP_2) SS_TAP(X_KP_7) SS_TAP(X_KP_9) SS_TAP(X_KP_9) SS_TAP(X_KP_5) ));
     }
     break;
-    case FINGERS_XED:
+    case FINGERS_XED_LT:
     if (record->event.pressed) {
       SEND_STRING(SS_LALT(SS_TAP(X_KP_1) SS_TAP(X_KP_2) SS_TAP(X_KP_9) SS_TAP(X_KP_3) SS_TAP(X_KP_1) SS_TAP(X_KP_0) ) SS_LALT(SS_TAP(X_KP_1) SS_TAP(X_KP_2) SS_TAP(X_KP_7) SS_TAP(X_KP_9) SS_TAP(X_KP_9) SS_TAP(X_KP_5) ));
     }
